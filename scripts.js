58e0e0f343dc40ad49a37bdd02d053af706b4996
Kelan Blash <73241853+KB-Kodes13@users.noreply.github.com>
2023-05-02 12:48:48 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,12 @@ let input = document.getElementById("input");
 let msg = document.getElementById("msg"); 
 let posts = document.getElementById("posts"); 
 
+form.addEventListener("submit", function(click) {
+  click.preventDefault();
+  console.log("button clicked");
+  formValidation();
+});
+
 
 let data = {};
 
