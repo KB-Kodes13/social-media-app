bfc25e26ed104fc04425685422faa20ed20bd193
ekdrobinski <127761998+ekdrobinski@users.noreply.github.com>
2023-05-02 12:01:18 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,3 +17,24 @@ function acceptData() {
   console.log(data);
   createPost();
 }
+
+function createPost() {
+  // gets html Element
+  const postElement = document.createElement("div");
+
+  //a paragraph element to display the post text
+  const textElement = document.createElement("p");
+  textElement.textContent = data.text;
+
+  //Uses a span element for post options
+  const optionsElement = document.createElement("span");
+  optionsElement.classList.add("options");
+
+  //creates an edit button and attaches an event listener
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.classList.add("edit");
+  editButton.addEventListener("click", function() {
+    editPost(this);
+  });
+
