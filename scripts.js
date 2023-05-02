ccb94c7d433960863158e86d6043203f7cbfb3fb
ongvang00 <127782062+ongvang00@users.noreply.github.com>
2023-05-02 12:39:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg"); 
 let posts = document.getElementById("posts"); 
+
+
+let data = {};
+
+function acceptData() {
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+}
