50c920a173cb8bd9c1f0cdba104b88dc4c54277a
Kelan Blash <kjblash13@gmail.com>
2023-05-03 14:32:01 -0400

    button functionality commit also changed your post here to timeline in the html file

--- heart.js
+++ heart.js
@@ -1,23 +1,27 @@
-const likeBtn = document.querySelector('.heart-icon');
-const numberOfLikesElement = document.querySelector('.number-of-likes');
-let numberOfLikes = Number.parseInt(numberOfLikesElement.textContent, 10);
-let isLiked = false;
-// Functions
-const likeClick = () => {
-  if (!isLiked) {
-  likeBtn.classList.add('isLiked');
-  likeBtn.textContent = '💖'; /* Change the heart icon to sparkling heart */
-  numberOfLikes++;
-  numberOfLikesElement.textContent = numberOfLikes;
-  isLiked = !isLiked;
-  } else {
-  likeBtn.classList.remove('isLiked');
-  likeBtn.textContent = '🤍'; /* Change the heart icon back to white heart */
-  numberOfLikes--;
-  numberOfLikesElement.textContent = numberOfLikes;
-  isLiked = !isLiked;
- }
-};
-// Event Listeners
-likeBtn.addEventListener('click', likeClick);
+const likeBtns = document.querySelectorAll('.heart-icon');
+const numberOfLikesElements = document.querySelectorAll('.number-of-likes');
+
+likeBtns.forEach((likeBtn, index) => {
+  let numberOfLikes = Number.parseInt(numberOfLikesElements[index].textContent, 10);
+  let isLiked = false;
+  
+  const likeClick = () => {
+    if (!isLiked) {
+      likeBtn.classList.add('isLiked');
+      likeBtn.textContent = '💖';
+      numberOfLikes++;
+      numberOfLikesElements[index].textContent = numberOfLikes;
+      isLiked = !isLiked;
+    } else {
+      likeBtn.classList.remove('isLiked');
+      likeBtn.textContent = '🤍';
+      numberOfLikes--;
+      numberOfLikesElements[index].textContent = numberOfLikes;
+      isLiked = !isLiked;
+    }
+  };
+  //Event Listener
+  likeBtn.addEventListener('click', likeClick);
+});
+
 // https://jssecrets.com/how-to-create-javascript-like-button/
