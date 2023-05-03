913065186aaf45122a6fbb0a951476b34ea7bdff
ongvang00 <127782062+ongvang00@users.noreply.github.com>
2023-05-03 16:20:59 -0400

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -1,1 +1,24 @@
 
+
+const noteForm = document.getElementById("note-form");
+const noteInput = document.getElementById("find");
+const noteSubmit = document.getElementById("note-submit");
+const notes = document.getElementById("notes");
+
+let notesStorage = localStorage.getItem("notes")
+  ? JSON.parse(localStorage.getItem("notes"))
+  : [];
+
+  noteForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    notesStorage.push(noteInput.value);
+    localStorage.setItem("notes", JSON.stringify(notesStorage));
+    listBuilder(noteInput.value);
+    noteInput.value = "";
+  });
+
+  const listBuilder = (text) => {
+    const note = document.createElement("li");
+    note.innerHTML = text + ' <button onclick="deleteNote(this)">x</button>';
+    notes.appendChild(note);
+  };
